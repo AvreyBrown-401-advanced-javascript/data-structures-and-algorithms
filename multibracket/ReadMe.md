79b90c74526a0ab2bc512c1d30ed6ee0a92379bb
Avrey Brown <avreyjbrown@yahoo.com>
2019-10-31 10:16:40 -0700

    notDone

---
+++ multibracket/ReadMe.md
@@ -0,0 +1,13 @@
+# Reverse an Array
+<!-- Short summary or background information -->
+Cahllenge is to return true or false based on if the string has matching brackets, despite non bracket characters 
+
+## Challenge
+<!-- Description of the challenge -->
+Write a function that takes a string as its only argument, and should return a boolean representing whether or not the brackets in the string are balanced
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+I planned everything out via whiteboard, and afterwards tested my code on repl
+
+## Solution
+<!-- Embedded whiteboard image -->
---
+++ multibracket/multiBracket.test.js
@@ -0,0 +1,54 @@
+'use strict';
+
+require('./multiBracket');
+
+let multiBracket = function (str) {
+  let array = [];
+  let pairs = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  }
+
+  for (let i = 0; i < str.length; i++) {
+
+    if (str[i] === '(' || str[i] === '{' || str[i] === '[' ) {
+      array.push(str[i]);
+    } else {
+      let last = array.pop();
+      if (str[i] !== pairs[last]) {return false};
+    }
+  }
+  if (array.length !== 0) {return false}
+  return true;
+}
+
+describe('the strings will return true if the brackets all have a match ', () => {
+  it('will return true beacuse all of the brakets ar matched',  () => {
+    expect(multiBracket('{}')).toBeTruthy();
+  })
+});
+
+describe('the strings will return true if the brackets all have a match ', () => {
+  it('will return true beacuse all of the brakets ar matched',  () => {
+    expect(multiBracket('{}(){}')).toBeTruthy();
+  })
+});
+
+describe('the strings will return true if the brackets all have a match ', () => {
+  it('will return true beacuse all of the brakets ar matched',  () => {
+    expect(multiBracket('()[[Extra Characters]]')).toBeTruthy();
+  })
+});
+
+describe('the strings will return true if the brackets all have a match ', () => {
+  it('will return true beacuse all of the brakets ar matched',  () => {
+    expect(multiBracket('()')).toBeTruthy();
+  })
+});
+
+describe('the strings will return true if the brackets all have a match ', () => {
+  it('will return true beacuse all of the brakets ar matched',  () => {
+    expect(multiBracket('()')).toBeTruthy();
+  })
+});

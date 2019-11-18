e6ccc0a908ea133737d3f3c11f99a60e5ee0d109
Avrey Brown <avreyjbrown@yahoo.com>
2019-11-18 15:16:58 -0800

    challenge26

---
+++ blog/blog.md
@@ -0,0 +1,3 @@
+# Blog Post for Code Challenge 26
+
+- the function insertionSort traverses a given array and stores all of its values into a temp array
---
+++ blog/blog.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+describe('the test will output the sorted array', () => {
+  it('will return true when compared to the sorted array', () => {
+    expect(insertionSort([8,4,23,42,16,15])).toBeEqualTo()
+  }) 
+})
